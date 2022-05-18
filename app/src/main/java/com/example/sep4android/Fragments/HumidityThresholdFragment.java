dcf5c6d975310424142a56a9d52cc980edaa03c4
BiancaMilitaru <bianca.militaru@yahoo.com>
2022-05-18 09:40:45 +0200

    Fragment humidity threshold list + item done

--- app/src/main/java/com/example/sep4android/Fragments/HumidityThresholdFragment.java
+++ app/src/main/java/com/example/sep4android/Fragments/HumidityThresholdFragment.java
@@ -1,9 +1,12 @@
 package com.example.sep4android.Fragments;
 
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,6 +15,7 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
+import com.example.sep4android.Adapters.HumidityThresholdAdapter;
 import com.example.sep4android.Objects.HumidityThresholdObject;
 import com.example.sep4android.Objects.Room;
 import com.example.sep4android.R;
@@ -26,15 +30,29 @@ public class HumidityThresholdFragment extends Fragment implements AdapterView.O
     private View view;
     private RoomViewModel viewModel;
     private ArrayList<String> mCountryList;
+    private RecyclerView humidityThresholdList;
+    private HumidityThresholdAdapter humidityThresholdAdapter;
+    private Context context;
+
 
     public HumidityThresholdFragment() {
     }
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        context = container.getContext();
         view = inflater.inflate(R.layout.fragment_humidity_threshold_list, container, false);
         viewModel = new ViewModelProvider(requireActivity()).get(RoomViewModel.class);
         viewModel.getRooms().observe(getViewLifecycleOwner(), listObjects -> initList(listObjects));
+
+        humidityThresholdList = view.findViewById(R.id.humidity_threshold_list);
+        humidityThresholdList.hasFixedSize();
+        humidityThresholdList.setLayoutManager(new LinearLayoutManager(this.getContext()));
+
+        humidityThresholdAdapter = new HumidityThresholdAdapter();
+        humidityThresholdList.setAdapter(humidityThresholdAdapter);
+
         return view;
     }
 
@@ -43,9 +61,9 @@ public class HumidityThresholdFragment extends Fragment implements AdapterView.O
         for (Room object : listObjects) {
             mCountryList.add(object.getRoomId());
         }
-        Spinner spinner = view.findViewById(R.id.sp);
+        Spinner spinner = view.findViewById(R.id.sp_humidity);
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this.getActivity(), R.layout.spin_item, mCountryList);
-        // SpinnerRoomAdapter adapter = new SpinnerRoomAdapter(requireActivity(),android.R.layout.simple_spinner_item, mCountryList);
+        //SpinnerRoomAdapter adapter = new SpinnerRoomAdapter(requireActivity(),android.R.layout.simple_spinner_item, mCountryList);
         adapter.setDropDownViewResource(R.layout.spin_item_dropdown);
         spinner.setAdapter(adapter);
         spinner.setOnItemSelectedListener(this);
