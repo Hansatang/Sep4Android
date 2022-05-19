cc8743b23aca817350879ea51bc5ae9e175ce43d
Tolnai <tolnai.milan22@gmail.com>
2022-05-19 19:30:54 +0200

    Some changes and created the TemperatureThresholdViewModel class

---
+++ app/src/main/java/com/example/sep4android/ViewModels/TemperatureThresholdViewModel.java
@@ -0,0 +1,14 @@
+package com.example.sep4android.ViewModels;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+
+public class TemperatureThresholdViewModel extends AndroidViewModel {
+
+
+    public TemperatureThresholdViewModel(@NonNull Application application) {
+        super(application);
+    }
+}
